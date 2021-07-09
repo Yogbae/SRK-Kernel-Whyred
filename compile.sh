f3a6fa46c1551881521eb04e0db2652410987e11
WhysDev <septianriya@gmail.com>
2021-07-08 19:03:47 -0800

    compile

---
+++ compile.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+export KBUILD_BUILD_USER=WhysDev
+export KBUILD_BUILD_HOST=SRK
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+# Compie plox
+function compile() {
+    make -j$(nproc) O=out ARCH=arm64 whyred_defconfig
+    make -j$(nproc) ARCH=arm64 O=out \
+                              CC=clang-11
+}
+compile
