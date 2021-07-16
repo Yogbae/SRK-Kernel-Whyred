b436db570be5fbdc0f5b2ad811e7da43e7e938e0
WhysDev <septianriya@gmail.com>
2021-07-16 11:41:49 +0700

    drone

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export KERNELNAME="SRK-Kernel-V14
+
+export LOCALVERSION=V14
+
+export KBUILD_BUILD_USER=WhysDev
+
+export KBUILD_BUILD_HOST=SRK
+
+export TOOLCHAIN=clang
+
+export DEVICES=whyred
+
+source helper
+
+gen_toolchain
+
+send_msg "Start building ${KERNELNAME} ${LOCALVERSION} for ${DEVICES}..."
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do
+	build ${i} -oldcam
+
+	build ${i} -newcam
+done
+
+END=$(date +"%s")
+
+DIFF=$(( END - START ))
+
+send_msg "Build succesfully in $((DIFF / 60))m $((DIFF % 60))s"
